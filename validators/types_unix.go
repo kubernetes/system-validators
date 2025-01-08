9a4d5f685fea762bbda1726c8d49e4197fe087c1
Paco Xu <paco.xu@daocloud.io>
2025-01-08 16:49:35 +0800

    Update versions note
    Co-authored-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- validators/types_unix.go
+++ validators/types_unix.go
@@ -30,7 +30,7 @@ var DefaultSysSpec = SysSpec{
 	KernelSpec: KernelSpec{
 		// 5.4, 5.10, 5.15 is an active kernel Long Term Support (LTS) release, tracked in https://www.kernel.org/category/releases.html.
 		Versions:     []string{`^5\.4.*$`, `^5\.10.*$`, `^5\.15.*$`, `^([6-9]|[1-9][0-9]+)\.([0-9]+)\.([0-9]+).*$`},
-		VersionsNote: "LTS versions from the 5.x series are 5.4, 5.10 and 5.15. Any 6.x versions are also supported. For cgroups v2 support, the minimal version is 4.15 and the recommended version is 5.8+",
+		VersionsNote: "Supported LTS versions from the 5.x series are 5.4, 5.10 and 5.15. Any 6.x version is also supported. For cgroups v2 support, the recommended version is 5.10 or newer",
 		// TODO(random-liu): Add more config
 		// TODO(random-liu): Add description for each kernel configuration:
 		Required: []KernelConfig{
